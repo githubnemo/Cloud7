413931dcccb2a5c069e8f8675c243a78e1af19ce
Marian <nemo@ikkoku.de>
2011-05-11 20:11:07 +0200

    Add stub configuration module

---
+++ core/lib/conf/conf.js
@@ -0,0 +1,41 @@
+/*
+var fs = require('fs');
+
+var username = process.ENV['USER'];
+var base_properties_file = process.cwd() + '/properties/build.json';
+var user_properties_file = process.cwd() + '/properties/' + username + '.json';
+var properties = {};
+var user_properties = {};
+
+try {
+	properties = JSON.parse(fs.readFileSync(base_properties_file, 'utf8'));
+} catch (err){
+	console.log(err);
+}
+
+try {
+	user_properties = JSON.parse(fs.readFileSync(user_properties_file, 'utf8'));
+} catch (err){
+	// do nothing and don't override
+}
+
+for (attrname in user_properties) {
+	properties[attrname] = user_properties[attrname];
+}
+
+module.exports = properties;
+*/
+
+function getModule(LocalModule) {
+
+	var ConfModule = function(name, methods, obj) {
+		LocalModule.apply(this, [name, methods, obj]);
+	}
+
+	ConfModule.prototype = {};
+
+	return ConfModule;
+
+}
+
+module.exports = {getModule: getModule};
